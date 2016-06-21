99b0b182a223cd25987f92812673aa23ccaf7ad0
Dylan Castanhinha <dcastanrn@gmail.com>
2016-06-21 09:52:26 -0400

    Added toolbar to bottom but the buttons do not function

---
+++ ParseStarterProject/BottomToolbar.swift
@@ -0,0 +1,21 @@
+//
+//  BottomToolbar.swift
+//  ParseStarterProject
+//
+//  Created by Dylan Castanhinha on 6/21/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import UIKit
+
+class BottomToolbar: UIToolbar {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
